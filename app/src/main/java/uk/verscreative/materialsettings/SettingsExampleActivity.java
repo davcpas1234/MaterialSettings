f93af2fc6cc398a6716b3b3264fcdea9d7129301
Priyank Patel <1priyank1@users.noreply.github.com>
2016-10-19 18:31:47 +0530

    Fixed : ClassCastException in nested preference screens Android API 24
    Fixed : ClassCastException in nested preference screens Android N API 24

--- app/src/main/java/uk/verscreative/materialsettings/SettingsExampleActivity.java
+++ app/src/main/java/uk/verscreative/materialsettings/SettingsExampleActivity.java
@@ -218,8 +218,12 @@ public class SettingsExampleActivity extends PreferenceActivity {
         View listRoot = dialog.findViewById(android.R.id.list);
         ViewGroup mRootView = (ViewGroup) dialog.findViewById(android.R.id.content);
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            LinearLayout root = (LinearLayout) dialog.findViewById(android.R.id.list).getParent().getParent();
+            appBar = (AppBarLayout) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
+            root.addView(appBar, 0);
+        }
+        else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
             LinearLayout root = (LinearLayout) dialog.findViewById(android.R.id.list).getParent();
             appBar = (AppBarLayout) LayoutInflater.from(this).inflate(R.layout.toolbar_settings, root, false);
             root.addView(appBar, 0);
